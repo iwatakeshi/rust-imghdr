548f8498c05781058d695696a39490c8b1dd1271
svartalf <self@svartalf.info>
2016-07-18 13:26:22 +0300

    Update documentation

---
+++ imghdr/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Type","Recognized image types"]],"fn":[["open","Open file and test if it an image."],["what","Tests the image data contained in the `f` bytes stream."]]});
---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["imghdr"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='imghdr/enum.Type.html' title='imghdr::Type'>Type</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
